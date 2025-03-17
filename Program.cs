4b5452459897d113fa034348517485660ccfdb7e
Beatriz Batista <byah24@outlook.com>
2025-03-17 10:22:48 -0300

    Programa finalizado pra lista

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.DarkGray;
+
+Console.WriteLine("       |\\      _,,,---,,_");
+Console.WriteLine("ZZZzz /,`.-'`'    -.  ;-;;");
+Console.WriteLine("     |,4-  ) )-,_. ,\\ (  `'");
+Console.WriteLine(" '---''(_/--'  `-'\\_)  Felix Lee Reproduzida by Bia Batista");
+
+Console.ResetColor();
